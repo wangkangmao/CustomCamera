dc118345fa85d0fb853afe3e3b483b8d15526c9f
wangkangmao <1240413544@qq.com>
2016-12-05 13:00:45 +0800

    自定义相机

--- app/src/main/java/com/github/wangkangmao/MainActivity.java
+++ app/src/main/java/com/github/wangkangmao/MainActivity.java
@@ -1,13 +1,76 @@
 package com.github.wangkangmao;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int REQ_1 = 0;
+    private static int REQ_2 = 1;
+    private ImageView mImageView;
+    private String mFilePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
+        Button customCamera = (Button) findViewById(R.id.custom_camera);
+        mImageView = (ImageView) findViewById(R.id.iv);
+        customCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customCamera();
+            }
+        });
+    }
+
+    private void initData() {
+        mFilePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFilePath = mFilePath + "/" + "temp.png";
+    }
+
+
+    private void customCamera() {
+        startActivity(new Intent(this, CustomCameraActivity.class));
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQ_1) {
+                Bundle bundle = data.getExtras();
+                Bitmap bitmap = (Bitmap) bundle.get("data");
+                mImageView.setImageBitmap(bitmap);
+            } else if (requestCode == REQ_2) {
+                FileInputStream fis = null;
+                try {
+                    fis = new FileInputStream(mFilePath);
+                    Bitmap bitmap = BitmapFactory.decodeStream(fis);
+                    mImageView.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
